179be0d31b58d33cf8d6d41593183d334c6f8a24
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-05-09 16:21:53 +0000

    changed js

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-05-HTML/sw.js", {
-    scope: "/ICS2O-Unit5-05-HTML/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-06-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-06-HTML/",
   })
 }
 
